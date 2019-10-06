f9d63708f1b065afc4cc7433f31db50a51c0e50c
dyacob <yacob@geez.org>
2019-10-06 06:56:19 -0400

    conversion of row to first level of json object

--- src/main/java/org/geez/ui/xliterator/JsonIndexGenerator.java
+++ src/main/java/org/geez/ui/xliterator/JsonIndexGenerator.java
@@ -67,17 +67,17 @@ public class JsonIndexGenerator {
 		JsonObject scripts = new JsonObject();
 		index.add( "Scripts", scripts );
 		
-		int FILE           = 0;
-		int SCRIPT_IN      = 1;
-		int VARIANT_IN     = 2;
-		int SCRIPT_OUT     = 3;
-		int VARIANT_OUT    = 4;
-		int DIRECTION      = 5;
-		int ALIAS          = 6;
-		int BACKWARD_ALIAS = 7;
-		int VISIBILITY     = 8;
-		int DEPENDENCIES   = 9;
-		int TOOLTIP        =10;
+		int FILE           = 0; // A
+		int SCRIPT_IN      = 1; // B
+		int VARIANT_IN     = 2; // C
+		int SCRIPT_OUT     = 3; // D
+		int VARIANT_OUT    = 4; // E
+		int DIRECTION      = 5; // F
+		int ALIAS          = 6; // G
+		int BACKWARD_ALIAS = 7; // H
+		int VISIBILITY     = 8; // I
+		int DEPENDENCIES   = 9; // J
+		int TOOLTIP        =10; // K
 		
 		List<Row> rows = data.getRow();
 		rows.remove(0); //shift
@@ -85,38 +85,67 @@ public class JsonIndexGenerator {
 		for (Row r : rows ) {
 			System.out.println("row " + r.getR() );		
 			
-			List<Cell> columns = r.getC();
+			String file = null, scriptIn = null, variantIn = null, scriptOut = null,
+					variantOut = null, direction = null, alias = null, backwardAlias = null,
+					dependencies = null, visibility = null, tooltip = null;
 			
-			String file      = formatter.formatCellValue( columns.get(FILE) );
-			String scriptIn  = formatter.formatCellValue( columns.get(SCRIPT_IN) );
-
-			if( columns.get(VARIANT_IN) != null ) {
-				String variantIn = formatter.formatCellValue( columns.get(VARIANT_IN) );
-			}			
-			String scriptOut = formatter.formatCellValue( columns.get(SCRIPT_OUT) );
-			if( columns.get(VARIANT_OUT) != null ) {
-				String variantOut = formatter.formatCellValue( columns.get(VARIANT_OUT) );
-			}
-
 			
-			String direction = formatter.formatCellValue( columns.get(DIRECTION) );
-			if( columns.get(ALIAS) != null ) {
-				String alias = formatter.formatCellValue( columns.get(ALIAS) );
+			for( Cell c: r.getC() ) {
+
+				switch( c.getR().charAt(0) ) {
+					case 'A':
+						file      = formatter.formatCellValue( c );
+						break;
+						
+					case 'B':
+						scriptIn  = formatter.formatCellValue( c );
+						break;
+						
+					case 'C':
+						variantIn = formatter.formatCellValue( c );
+						break;
+						
+					case 'D':
+						scriptOut = formatter.formatCellValue( c );
+						break;
+						
+					case 'E':
+						variantOut = formatter.formatCellValue( c );
+						break;
+						
+					case 'F':
+						direction = formatter.formatCellValue( c );
+						break;
+						
+					case 'G':
+						alias = formatter.formatCellValue( c );
+						break;
+							
+					case 'H':
+						backwardAlias = formatter.formatCellValue( c );
+						break;
+						
+					case 'I':
+						dependencies = formatter.formatCellValue( c );
+						break;
+					
+					case 'J':
+						visibility = formatter.formatCellValue( c );
+						break;
+						
+					case 'K':
+						tooltip = formatter.formatCellValue( c );
+						break;	
+				}
 			}
-			if( columns.get(BACKWARD_ALIAS) != null ) {
-				String backwardAlias = formatter.formatCellValue( columns.get(BACKWARD_ALIAS) );
-			}
-			if( columns.get(DEPENDENCIES) != null ) {
-				String dependencies = formatter.formatCellValue( columns.get(DEPENDENCIES) );
-			}
-			if( columns.get(VISIBILITY) != null ) {
-				String visibility = formatter.formatCellValue( columns.get(VISIBILITY) );
-			}
-			if( columns.get(TOOLTIP) != null ) {
-				String tooltip = formatter.formatCellValue( columns.get(TOOLTIP) );
+
+			if(! index.has( scriptIn ) ) {
+				index.add( scriptIn, new JsonObject() );
 			}
 
 		}
+		
+		System.err.println( index.toString() );
 		
 	}
 	
